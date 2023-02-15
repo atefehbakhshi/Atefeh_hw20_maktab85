8cea11b74a1a895e304f8f35b58fe6137e6b1bea
Atefeh <at.bakhshi1373@gmail.com>
2023-02-15 07:08:27 +0330

    feat:import form component in to app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Form from "./components/form";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen p-4">
+      <Form />
+    </div>
+  );
+}
+
+export default App;

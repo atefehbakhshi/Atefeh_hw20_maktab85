b6621d5fc4ad89c02d4e88c5408c6da2c4ce1b81
Atefeh <at.bakhshi1373@gmail.com>
2023-02-15 07:04:15 +0330

    feat: separate validation expresion

---
+++ src/validation/validPatterns.ts
@@ -0,0 +1,4 @@
+export const emailPattern = "[^@ \t\r\n]+@[^@ \t\r\n]+.[^@ \t\r\n]+";
+export const namePattern = "^[a-zA-Z\u0600-\u06FFs ]+$";
+export const cooperationPositionPattern = "^[a-zA-Z\u0600-\u06FFs ]+$";
+export const phonePattern = "09[0-9]";
